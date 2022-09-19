80943349ae916c76eabda37c6200f8214f667320
sebastian krnjus <sebastiankrnjus25@gmail.com>
2022-09-19 11:48:39 +0200

    Add project files.

---
+++ KlasaProcs/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace KlasaProcs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonexp = new System.Windows.Forms.Button();
+            this.buttonExcel = new System.Windows.Forms.Button();
+            this.buttonWord = new System.Windows.Forms.Button();
+            this.buttonInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonexp
+            // 
+            this.buttonexp.Location = new System.Drawing.Point(126, 89);
+            this.buttonexp.Name = "buttonexp";
+            this.buttonexp.Size = new System.Drawing.Size(120, 23);
+            this.buttonexp.TabIndex = 0;
+            this.buttonexp.Text = "Internet explorer";
+            this.buttonexp.UseVisualStyleBackColor = true;
+            this.buttonexp.Click += new System.EventHandler(this.buttonexp_Click);
+            // 
+            // buttonExcel
+            // 
+            this.buttonExcel.Location = new System.Drawing.Point(306, 89);
+            this.buttonExcel.Name = "buttonExcel";
+            this.buttonExcel.Size = new System.Drawing.Size(120, 23);
+            this.buttonExcel.TabIndex = 1;
+            this.buttonExcel.Text = "Excel";
+            this.buttonExcel.UseVisualStyleBackColor = true;
+            this.buttonExcel.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // buttonWord
+            // 
+            this.buttonWord.Location = new System.Drawing.Point(126, 164);
+            this.buttonWord.Name = "buttonWord";
+            this.buttonWord.Size = new System.Drawing.Size(120, 23);
+            this.buttonWord.TabIndex = 2;
+            this.buttonWord.Text = "Word";
+            this.buttonWord.UseVisualStyleBackColor = true;
+            this.buttonWord.Click += new System.EventHandler(this.buttonWord_Click);
+            // 
+            // buttonInfo
+            // 
+            this.buttonInfo.Location = new System.Drawing.Point(306, 164);
+            this.buttonInfo.Name = "buttonInfo";
+            this.buttonInfo.Size = new System.Drawing.Size(120, 23);
+            this.buttonInfo.TabIndex = 3;
+            this.buttonInfo.Text = "StartInfo";
+            this.buttonInfo.UseVisualStyleBackColor = true;
+            this.buttonInfo.Click += new System.EventHandler(this.buttonInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 302);
+            this.Controls.Add(this.buttonInfo);
+            this.Controls.Add(this.buttonWord);
+            this.Controls.Add(this.buttonExcel);
+            this.Controls.Add(this.buttonexp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonexp;
+        private System.Windows.Forms.Button buttonExcel;
+        private System.Windows.Forms.Button buttonWord;
+        private System.Windows.Forms.Button buttonInfo;
+    }
+}
+
---
+++ KlasaProcs/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace KlasaProcs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("EXCEL.EXE");
+        }
+
+        private void buttonexp_Click(object sender, EventArgs e)
+        {
+            Process.Start("iexplore.exe");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("WINWORD.EXE");
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("iexplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "www.sser.hr";
+            Process.Start(startInfo);
+        }
+    }
+}
